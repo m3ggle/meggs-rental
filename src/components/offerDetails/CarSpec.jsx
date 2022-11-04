4ee3ba4fca3134baa370be554fcc3c6ccb6148f6
Meggle <megglebande@web.de>
2022-11-04 14:14:35 +0100

    car spec dark mode

--- src/components/offerDetails/CarSpec.jsx
+++ src/components/offerDetails/CarSpec.jsx
@@ -3,10 +3,12 @@ import React from "react";
 const CarSpec = ({title, value, icon}) => {
   return (
     <div className="flex h-[100px] w-[96px] flex-col items-center justify-center gap-y-2 rounded-lg py-3 shadow-md">
-      <div className={`${icon} flex h-6 w-6 items-center justify-center text-[24px] text-lmGrey300`}></div>
+      <div
+        className={`${icon} flex h-6 w-6 items-center justify-center text-[24px] text-lmGrey300 dark:text-dmGrey300`}
+      ></div>
       <div className="flex flex-col items-center justify-center gap-y-1 text-xs">
-        <span className="text-lmGrey300">{title}</span>
-        <span className="text-lmGrey600">{value}</span>
+        <span className="text-lmGrey300 dark:text-dmGrey300">{title}</span>
+        <span className="text-lmGrey600 dark:text-dmGrey100">{value}</span>
       </div>
     </div>
   );

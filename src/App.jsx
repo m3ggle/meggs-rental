5664d2428a516f480af795df5298cbf6c09d1473
Meggle <megglebande@web.de>
2022-11-05 17:10:08 +0100

    Outsource Header from UserProfileBig #64

--- src/App.jsx
+++ src/App.jsx
@@ -2,21 +2,28 @@ import DropdownMode from "./components/DropdownMode";
 // import Spline from "@splinetool/react-spline";
 import { useForm } from "react-hook-form";
 import ReviewSection from "./components/ratings/ReviewSection";
+import UserProfileBig from "./components/userProfile/UserProfileBig";
+import ExampleData from "./ExampleData";
 
 export default function App() {
   const { control, handleSubmit } = useForm();
   const onSubmit = (data) => console.log(data);
+
+  const {userProfileBig} = ExampleData()
 
   return (
     <div className="relative flex h-screen w-full flex-col items-center justify-center overflow-scroll bg-white dark:bg-dmGrey900">
       <div className="hidden">
         <DropdownMode />
       </div>
-
-      <div className="flex w-[360px] 600:w-[489px] justify-center rounded-2xl py-6 px-4 shadow overflow-scroll h-[640px]">
+      {/* <div className="flex w-[360px] 600:w-[489px] justify-center rounded-2xl py-6 px-4 shadow overflow-scroll h-[640px]">
         <ReviewSection />
-      </div>
+      </div> */}
 
+      <div className="w-[360px]">
+      <UserProfileBig userData={userProfileBig} />
+      </div>
+      
       {/* <MobileCatalog /> */}
     </div>
   );
--- src/components/userProfile/UserProfileBig.jsx
+++ src/components/userProfile/UserProfileBig.jsx
@@ -1,6 +1,7 @@
 // <UserProfileBig userData={userProfileBig} />
 import React from "react";
 import { getAge } from "../../utilities/getAge";
+import UserProfileHeader from "./UserProfileHeader";
 
 const UserProfileBig = ({ userData }) => {
   const {firstName, lastName, birthday, email, reviewsCount, joined, identityVerified} = userData
@@ -8,27 +9,7 @@ const UserProfileBig = ({ userData }) => {
   return (
     <div className="flex w-full flex-col items-center gap-y-2 rounded-3xl bg-white dark:bg-dmGrey900 p-6 shadow-md">
       {/* header */}
-      <div className="flex w-full flex-col items-center gap-y-2">
-        <div
-          className="h-[84px] w-[84px] rounded-full bg-cover bg-center shadow"
-          style={{
-            backgroundImage:
-              "url(https://images.unsplash.com/photo-1635107510862-53886e926b74?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2535&q=80)",
-          }}
-        ></div>
-        <div className="flex w-full flex-col items-center gap-y-[2px] text-sm text-lmGrey600 dark:text-dmGrey100">
-          <div className="flex w-full items-center justify-center gap-x-[2px]">
-            <span className="w-full truncate text-right text-lg text-lmGrey800 dark:text-dmGrey25">
-              {firstName} {lastName}
-            </span>
-            <div className="text-lg">•</div>
-            <span className="w-full truncate text-left text-sm">
-              {getAge(birthday)} years old
-            </span>
-          </div>
-          <span>{email}</span>
-        </div>
-      </div>
+      <UserProfileHeader firstName={firstName} lastName={lastName} birthday={birthday} email={email} />
 
       {/* mid */}
       <div className="flex w-full flex-col items-center justify-center text-sm text-lmGrey400 dark:text-dmGrey300">

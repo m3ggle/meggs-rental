9eb8f910b24f702662ceb97da386a56e654ef2fb
Meggle <megglebande@web.de>
2022-11-09 21:43:44 +0100

    signwrapper, sign in, sign up

--- src/pages/signIn/SignIn.jsx
+++ src/pages/signIn/SignIn.jsx
@@ -1,113 +1,103 @@
 import React from "react";
 import { Controller, useForm } from "react-hook-form";
+import { useNavigate } from "react-router-dom";
 import TextInput from "../../components/input/TextInput";
 import ProgressBar from "../../components/ProgressBar";
-import CarsStreet from "../../assets/img/carsStreet.jpg"
-import { useNavigate } from "react-router-dom";
+import SignWrapper from "../../components/SignWrapper";
+import styles from "../../style";
 
 const SignIn = () => {
   const { control, handleSubmit } = useForm();
   const onSubmit = (data) => console.log(data);
 
-  const navigate = useNavigate()
+  const navigate = useNavigate();
 
-  const handleHomeButtonClick = () => {
-    navigate("/homepage")
-  }
+  const handleSignUpClick = () => {
+    navigate("/sign-up");
+  };
 
   return (
-    <div className="relative flex w-full items-center overflow-scroll bg-white dark:bg-dmGrey900">
-      {/* info part */}
-      <div className="flex h-full w-full max-w-[560px 1000:max-w-none flex-col items-center gap-y-2 px-6 py-[28px]">
-        {/* puffer */}
-        <div className="h-[100px] w-full"></div>
-        {/* main */}
-        <div className="flex w-full max-w-[348px] flex-col gap-y-[48px]">
-          {/* title part */}
-          <div className="flex flex-col items-center justify-center gap-y-2 p-2">
-            <span className="text-2xl text-lmGrey800 ">Almost finished</span>
-            <ProgressBar amount={4} finished={2} />
+    <SignWrapper pic="https://images.unsplash.com/photo-1628437255792-911a5d23097e?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=774&q=80">
+      <div className="flex w-full max-w-[348px] flex-col gap-y-[48px]">
+        {/* title part */}
+        <div className="flex flex-col items-center justify-center gap-y-1 p-2">
+          <span className="text-2xl text-lmGrey800 dark:text-dmGrey25">
+            Welcome Back
+          </span>
+          <span className="text-base text-lmGrey600 dark:text-dmGrey300">Suspendisse et nunc fringilla in tempus.</span>
+        </div>
+        {/* main main */}
+        <div className="flex w-full max-w-[340px] flex-col gap-y-8">
+          {/* inputs */}
+          <div className="flex w-full flex-col gap-y-2">
+            <Controller
+              name="email"
+              control={control}
+              render={({ field, fieldState }) => (
+                <TextInput
+                  firstIcon="fa-solid fa-at"
+                  onChange={field.onChange}
+                  label="Email"
+                  placeholder="maxMustermann@web.de"
+                  value={field.value}
+                  onBlur={field.onBlur}
+                  error={fieldState.error}
+                />
+              )}
+              rules={{ required: "Email is required" }}
+            />
+            <Controller
+              name="password"
+              control={control}
+              render={({ field, fieldState }) => (
+                <TextInput
+                  firstIcon="fa-solid fa-lock"
+                  onChange={field.onChange}
+                  label="What is your last name?"
+                  placeholder="Bande"
+                  type="password"
+                  value={field.value}
+                  onBlur={field.onBlur}
+                  error={fieldState.error}
+                />
+              )}
+              rules={{
+                required: "Last name is required",
+                minLength: { value: 3, message: "At least 3 Characters" },
+                pattern: {
+                  value: /^[A-Za-z]+$/i,
+                  message: "Only letters are allowed",
+                },
+              }}
+            />
           </div>
-          {/* main main */}
-          <div className="flex w-full max-w-[340px] flex-col gap-y-8">
-            {/* inputs */}
-            <div className="flex w-full flex-col gap-y-2">
-              <Controller
-                name="email"
-                control={control}
-                render={({ field, fieldState }) => (
-                  <TextInput
-                    firstIcon="fa-solid fa-at"
-                    onChange={field.onChange}
-                    label="Email"
-                    placeholder="maxMustermann@web.de"
-                    value={field.value}
-                    onBlur={field.onBlur}
-                    error={fieldState.error}
-                  />
-                )}
-                rules={{ required: "Email is required" }}
-              />
-              <Controller
-                name="password"
-                control={control}
-                render={({ field, fieldState }) => (
-                  <TextInput
-                    firstIcon="fa-solid fa-lock"
-                    onChange={field.onChange}
-                    label="What is your last name?"
-                    placeholder="Bande"
-                    type="password"
-                    value={field.value}
-                    onBlur={field.onBlur}
-                    error={fieldState.error}
-                  />
-                )}
-                rules={{
-                  required: "Last name is required",
-                  minLength: { value: 3, message: "At least 3 Characters" },
-                  pattern: {
-                    value: /^[A-Za-z]+$/i,
-                    message: "Only letters are allowed",
-                  },
-                }}
-              />
-            </div>
-            {/* bottom */}
-            <div className="flex flex-col gap-y-2 text-sm">
-              <button
-                type="submit"
-                onClick={onSubmit}
-                className="flex items-center justify-center rounded-lg bg-lmPrimary py-3 px-4 text-white shadow-md">
-                Sign In
-              </button>
-              <button className="flex items-center justify-center rounded-lg bg-primary100 py-3 px-4 text-lmPrimary">
-                Sign In with Google
-              </button>
-              <span className="text-lmGrey500">
-                Already have an Account?{" "}
-                <span className="text-lmGrey500 underline underline-offset-2">
-                  Sign In instead
-                </span>
+          {/* bottom */}
+          <div className="flex flex-col gap-y-2 text-sm">
+            <button
+              type="submit"
+              onClick={onSubmit}
+              className="flex items-center justify-center rounded-lg bg-lmPrimary py-3 px-4 text-white shadow-md"
+            >
+              Sign In
+            </button>
+            <button
+              className={`flex items-center justify-center rounded-lg bg-primary100 dark:bg-transparent ${styles.darkModeBorder} py-3 px-4 text-lmPrimary duration-300 dark:text-lmGrey100 dark:hover:border-dmGrey600`}
+            >
+              Sign In with Google
+            </button>
+            <span className="text-lmGrey500 dark:text-dmGrey300">
+              Don't have a account?{" "}
+              <span
+                onClick={handleSignUpClick}
+                className="cursor-pointer underline underline-offset-2 duration-300 dark:hover:text-dmGrey100"
+              >
+                Sign Up instead
               </span>
-            </div>
+            </span>
           </div>
         </div>
       </div>
-
-      {/* pic part */}
-      <div className="relative h-screen w-full p-3 1000:flex hidden">
-        <div
-          className="h-full w-full rounded-[30px] bg-lmGrey100 bg-cover bg-center"
-          style={{ backgroundImage: `url(${CarsStreet})` }}
-        />
-        <button
-          onClick={handleHomeButtonClick}
-          className="absolute top-[56px] right-[76px] flex h-[64px] w-[64px] items-center justify-center rounded-full bg-white">
-          <i className="fa-solid fa-house text-[28px] text-lmGrey800"></i>
-        </button>
-      </div>
-    </div>
+    </SignWrapper>
   );
 };
 
--- src/pages/signUp/SignUp.jsx
+++ src/pages/signUp/SignUp.jsx
@@ -1,9 +1,285 @@
-import React from 'react'
+import React, { useState } from "react";
+import { Controller, useForm } from "react-hook-form";
+import { useNavigate } from "react-router-dom";
+import Select from "../../components/input/Select";
+import TextInput from "../../components/input/TextInput";
+import ProgressBar from "../../components/ProgressBar";
+import SignWrapper from "../../components/SignWrapper";
+import ExampleData from "../../ExampleData";
+import styles from "../../style";
+
+const round = [
+  {
+    title: "Create a new account",
+  },
+  {
+    title: "What is your full name",
+  },
+  {
+    title: "Almost finished",
+  },
+  {
+    title: "Last step, promised 🤞",
+  },
+];
+
+const { genderSelect } = ExampleData();
 
 const SignUp = () => {
-  return (
-    <div>SignUp</div>
-  )
-}
+  const { control, handleSubmit } = useForm();
+  const onSubmit = (data) => console.log(data);
 
-export default SignUp
+  const [currentRound, setCurrentRound] = useState(0);
+
+  const navigate = useNavigate();
+
+  const handleSignInClick = () => {
+    navigate("/sign-in");
+  };
+
+  const handleContinue = () => {
+    setCurrentRound((prevState) => prevState + 1);
+  };
+
+  const handleGoBack = () => {
+    currentRound !== 0 && setCurrentRound((prevState) => prevState - 1);
+  };
+
+  const handleGoogle = () => {};
+
+  return (
+    <SignWrapper pic="https://images.unsplash.com/photo-1628437255792-911a5d23097e?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=774&q=80">
+      <div className="flex w-full max-w-[348px] flex-col gap-y-[48px]">
+        {/* title part */}
+        <div className="flex flex-col items-center justify-center gap-y-2 p-2">
+          <span className="text-2xl text-lmGrey800 dark:text-dmGrey25">
+            {round[currentRound].title}
+          </span>
+          <ProgressBar amount={round.length} finished={currentRound} />
+        </div>
+        {/* main main */}
+        <form
+          onSubmit={handleSubmit(onSubmit)}
+          className="flex w-full max-w-[340px] flex-col gap-y-8"
+        >
+          {/* inputs */}
+          <div className="flex w-full flex-col gap-y-2">
+            <div className={currentRound === 0 ? "flex" : "hidden"}>
+              <Controller
+                name="email"
+                control={control}
+                rules={{
+                  required: "Email is required",
+                  pattern: {
+                    value: /^\w+@[a-zA-Z_]+?\.[a-zA-Z]{2,3}$/,
+                    message: "Invalid email address",
+                  },
+                }}
+                render={({ field, fieldState }) => (
+                  <TextInput
+                    firstIcon="fa-solid fa-at"
+                    onChange={field.onChange}
+                    label="Email"
+                    placeholder="maxMustermann@web.de"
+                    value={field.value}
+                    onBlur={field.onBlur}
+                    error={fieldState.error}
+                  />
+                )}
+              />
+            </div>
+            <div className={currentRound === 0 ? "flex" : "hidden"}>
+              <Controller
+                name="password"
+                control={control}
+                rules={{
+                  required: "Password is required",
+                  pattern: {
+                    value:
+                      /^\S*(?=\S{6,})(?=\S*\d)(?=\S*[A-Z])(?=\S*[a-z])(?=\S*[!@#$%^&*? ])\S*$/,
+                    message:
+                      "Minimum 6 Characters - 1 upper and 1 lower case - 1 letter and 1 special character",
+                  },
+                }}
+                render={({ field, fieldState }) => (
+                  <TextInput
+                    firstIcon="fa-solid fa-lock"
+                    onChange={field.onChange}
+                    label="Password"
+                    placeholder="••••••"
+                    type="password"
+                    value={field.value}
+                    onBlur={field.onBlur}
+                    error={fieldState.error}
+                  />
+                )}
+              />
+            </div>
+            <div className={currentRound === 1 ? "flex" : "hidden"}>
+              <Controller
+                name="firstName"
+                control={control}
+                rules={{
+                  required: "First name is required",
+                  pattern: {
+                    value: /^[a-zA-Z]+$/,
+                    message: "Only letter are allowed",
+                  },
+                }}
+                render={({ field, fieldState }) => (
+                  <TextInput
+                    firstIcon="fa-solid fa-signature"
+                    onChange={field.onChange}
+                    label="First Name"
+                    placeholder="Max"
+                    type="text"
+                    value={field.value}
+                    onBlur={field.onBlur}
+                    error={fieldState.error}
+                  />
+                )}
+              />
+            </div>
+            <div className={currentRound === 1 ? "flex" : "hidden"}>
+              <Controller
+                name="lastName"
+                control={control}
+                rules={{
+                  required: "Last name is required",
+                  pattern: {
+                    value: /^[a-zA-Z]+$/,
+                    message: "Only letter are allowed",
+                  },
+                }}
+                render={({ field, fieldState }) => (
+                  <TextInput
+                    firstIcon="fa-solid fa-signature"
+                    onChange={field.onChange}
+                    label="Last Name"
+                    placeholder="Musterman"
+                    type="text"
+                    value={field.value}
+                    onBlur={field.onBlur}
+                    error={fieldState.error}
+                  />
+                )}
+              />
+            </div>
+            <div className={currentRound === 2 ? "flex" : "hidden"}>
+              <Controller
+                name="birthday"
+                control={control}
+                rules={{
+                  required: "Birthday is required",
+                }}
+                render={({ field, fieldState }) => (
+                  <TextInput
+                    firstIcon="fa-solid fa-calendar-days"
+                    onChange={field.onChange}
+                    label="Birthday"
+                    placeholder="25.10.1999"
+                    type="date"
+                    value={field.value}
+                    onBlur={field.onBlur}
+                    error={fieldState.error}
+                  />
+                )}
+              />
+            </div>
+            <div className={currentRound === 2 ? "flex" : "hidden"}>
+              <Controller
+                name="gender"
+                control={control}
+                rules={{ required: "Select an option" }}
+                render={({ field, fieldState }) => (
+                  <Select
+                    icon={genderSelect.icon}
+                    placeholder={genderSelect.placeholder}
+                    itemList={genderSelect.list}
+                    onChange={field.onChange}
+                    label="What is your gender?"
+                    error={fieldState.error}
+                  />
+                )}
+              />
+            </div>
+            <div className={currentRound === 2 ? "flex" : "hidden"}>
+              <Controller
+                name="city"
+                control={control}
+                rules={{
+                  required: "City is required",
+                }}
+                render={({ field, fieldState }) => (
+                  <TextInput
+                    firstIcon="fa-solid fa-city"
+                    onChange={field.onChange}
+                    label="City you live in"
+                    placeholder="Dresden"
+                    type="text"
+                    value={field.value}
+                    onBlur={field.onBlur}
+                    error={fieldState.error}
+                  />
+                )}
+              />
+            </div>
+            <div className={currentRound === 3 ? "flex" : "hidden"}>
+              <Controller
+                name="telephoneNumber"
+                control={control}
+                rules={{
+                  pattern: {
+                    value: /^\d+$/,
+                    message: "Only numbers are allowed",
+                  },
+                }}
+                render={({ field, fieldState }) => (
+                  <TextInput
+                    firstIcon="fa-solid fa-phone"
+                    onChange={field.onChange}
+                    label="Telephone number (optional)"
+                    placeholder="03 251 2342783"
+                    type="number"
+                    value={field.value}
+                    onBlur={field.onBlur}
+                    error={fieldState.error}
+                  />
+                )}
+              />
+            </div>
+          </div>
+          {/* bottom */}
+          <div className="flex flex-col gap-y-2 text-sm">
+            <button
+              type="submit"
+              onClick={
+                currentRound === round.length - 1 ? onSubmit : handleContinue
+              }
+              className="flex items-center justify-center rounded-lg bg-lmPrimary py-3 px-4 text-white shadow-md"
+            >
+              {currentRound === round.length - 1 ? "Finish" : "Continue"}
+            </button>
+            <button
+              onClick={currentRound === 0 ? handleGoogle : handleGoBack}
+              className={`flex items-center justify-center rounded-lg bg-primary100 dark:bg-transparent ${styles.darkModeBorder} py-3 px-4 text-lmPrimary duration-300 dark:text-lmGrey100 dark:hover:border-dmGrey600`}
+            >
+              {currentRound === 0 ? "Sign Up with Google" : "Go Back"}
+            </button>
+            <span className="text-lmGrey500 dark:text-dmGrey300">
+              Already have an Account?{" "}
+              <span
+                onClick={handleSignInClick}
+                className="cursor-pointer underline underline-offset-2 duration-300 dark:hover:text-dmGrey100"
+              >
+                Sign In instead
+              </span>
+            </span>
+          </div>
+        </form>
+      </div>
+    </SignWrapper>
+  );
+};
+
+export default SignUp;

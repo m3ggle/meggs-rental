52640889813654cab964830610526a57078dbfff
Meggle <megglebande@web.de>
2022-11-10 16:44:44 +0100

    profile sub: reviews

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,31 @@
 // import Spline from "@splinetool/react-spline";
 import { useState } from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import DropdownMode from "./components/DropdownMode";
 import Logo from "./components/Logo";
 import Navbar from "./components/Navbar";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Catalog from "./pages/catalog/Catalog";
-import Chat from "./pages/chat/Chat"
+import Chat from "./pages/chat/Chat";
+import ChatInfo from "./pages/chat/view/ChatInfo";
+import ChatMain from "./pages/chat/view/ChatMain";
+import ChatSidebar from "./pages/chat/view/ChatSidebar";
 import Favorites from "./pages/favorites/Favorites";
 import ForgotPassword from "./pages/forgetPassword/ForgotPassword";
 import Homepage from "./pages/homepage/Homepage";
 import Map from "./pages/map/Map";
-import NotFound from "./pages/notFound/NotFound"
+import NotFound from "./pages/notFound/NotFound";
 import OfferDetails from "./pages/offerDetails/OfferDetails";
-import Profile from "./pages/profile/Profile"
-import SignIn from "./pages/signIn/SignIn"
-import SignUp from "./pages/signUp/SignUp"
-import Upload from "./pages/upload/Upload"
-import UserOffers from "./pages/userOffers/UserOffers"
-import ChatInfo from "./pages/chat/view/ChatInfo"
-import ChatMain from "./pages/chat/view/ChatMain"
-import ChatSidebar from "./pages/chat/view/ChatSidebar"
-import DropdownMode from "./components/DropdownMode";
+import Profile from "./pages/profile/Profile";
 import Account from "./pages/profile/sub/Account";
-import Payments from "./pages/profile/sub/Payments";
-import Ratings from "./pages/profile/sub/Ratings";
 import Help from "./pages/profile/sub/Help";
-import TermsOfService from "./pages/profile/sub/TermsOfService";
+import Payments from "./pages/profile/sub/Payments";
 import PrivacyPolicy from "./pages/profile/sub/PrivacyPolicy";
+import Reviews from "./pages/profile/sub/Reviews";
+import TermsOfService from "./pages/profile/sub/TermsOfService";
+import SignIn from "./pages/signIn/SignIn";
+import SignUp from "./pages/signUp/SignUp";
+import Upload from "./pages/upload/Upload";
+import UserOffers from "./pages/userOffers/UserOffers";
 
 export default function App() {
   let [isOpen, setIsOpen] = useState(false);
@@ -48,7 +48,7 @@ export default function App() {
 
           <Route path="/user-offers" element={<UserOffers />} />
           <Route path="/upload" element={<Upload />} />
-          
+
           <Route path="/chat" element={<Chat />} />
           <Route path="/chat/sidebar" element={<ChatSidebar />} />
           <Route path="/chat/chat-info" element={<ChatInfo />} />
@@ -56,20 +56,22 @@ export default function App() {
 
           <Route path="/" element={<Homepage />} />
           <Route path="/homepage" element={<Homepage />} />
-          
+
           <Route path="/profile" element={<Profile />} />
           <Route path="/profile/account" element={<Account />} />
           <Route path="/profile/payments" element={<Payments />} />
-          <Route path="/profile/ratings" element={<Ratings />} />
+          <Route path="/profile/reviews" element={<Reviews />} />
           <Route path="/profile/help" element={<Help />} />
-          <Route path="/profile/terms-of-service" element={<TermsOfService />} />
+          <Route
+            path="/profile/terms-of-service"
+            element={<TermsOfService />}
+          />
           <Route path="/profile/privacy-policy" element={<PrivacyPolicy />} />
-          
+
           <Route path="/sign-in" element={<SignIn />} />
           <Route path="/sign-up" element={<SignUp />} />
           <Route path="/forgot-password" element={<ForgotPassword />} />
 
-          
           <Route path="/*" element={<NotFound />} />
         </Routes>
       </Router>

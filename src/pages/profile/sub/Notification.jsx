defc7fbfc0e69b7f71e138552accbd7ac2e26ff7
Meggle <megglebande@web.de>
2022-11-10 16:56:48 +0100

    edit profileButton

--- src/pages/profile/sub/Notification.jsx
+++ src/pages/profile/sub/Notification.jsx
@@ -1,9 +1,21 @@
 import React from 'react'
+import SignWrapper from '../../../components/SignWrapper';
+import ProfileHeader from '../view/ProfileHeader';
 
 const Notification = () => {
   return (
-    <div>Notification</div>
-  )
+    <SignWrapper
+      puffer={false}
+      pic="https://images.unsplash.com/photo-1658391157361-43b9984cbddf?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80"
+    >
+      <div className="flex w-full max-w-[348px] flex-col gap-y-3 overflow-y-scroll px-[2px] py-6">
+        <ProfileHeader title="Notification" />
+        <span className="w-full text-center text-lmGrey600">
+          Currently not available
+        </span>
+      </div>
+    </SignWrapper>
+  );
 }
 
 export default Notification
--- src/pages/profile/view/ProfileButton.jsx
+++ src/pages/profile/view/ProfileButton.jsx
@@ -2,21 +2,24 @@ import React from "react";
 import { useNavigate } from "react-router-dom";
 import styles from "../../../style";
 
-const ProfileButton = ({ btnTitle, icon, link }) => {
+const ProfileButton = ({ btnTitle, icon, link, secondIcon }) => {
   const navigate = useNavigate();
   const handleClick = () => navigate(link);
   return (
     <button
       type="button"
       onClick={handleClick}
-      className={`flex items-center justify-center gap-x-2 border border-solid border-lmGrey100 hover:scale-101 text-lmGrey300 hover:border-lmGrey300 active:scale-99 hover:text-lmGrey600 duration-300 ${styles.darkModeBorder} w-full rounded-lg px-4 py-3 shadow`}
+      className={`flex items-center justify-center gap-x-2 border border-solid border-lmGrey100 text-lmGrey300 duration-300 hover:scale-101 hover:border-lmGrey300 hover:text-lmGrey600 active:scale-99 ${styles.darkModeBorder} w-full rounded-lg px-4 py-3 shadow`}
     >
       <i
         className={`${icon} flex h-[14px] min-h-[14px] w-[14px] min-w-[14px] items-center justify-center text-[14px]`}
       />
-      <span className="w-full flex text-sm text-lmGrey600">{btnTitle}</span>
+      <span className="flex w-full text-sm text-lmGrey600">{btnTitle}</span>
       <i
-        className={`fa-solid fa-chevron-right flex h-[14px] min-h-[14px] w-[14px] min-w-[14px] items-center justify-center text-[14px]`}
+        className={`${
+          (secondIcon === undefined || secondIcon) &&
+          "fa-solid fa-chevron-right"
+        } flex h-[14px] min-h-[14px] w-[14px] min-w-[14px] items-center justify-center text-[14px]`}
       />
     </button>
   );

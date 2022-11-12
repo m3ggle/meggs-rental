6488d10ea8a0e523658d1a88c0071ab0f30f2882
Meggle <megglebande@web.de>
2022-11-12 12:26:31 +0100

    sign in, sign up, forgot... updated to new method

--- src/pages/forgetPassword/ForgotPassword.jsx
+++ src/pages/forgetPassword/ForgotPassword.jsx
@@ -1,42 +1,47 @@
 import React, { useState } from "react";
-import { Controller, useForm } from "react-hook-form";
 import { useNavigate } from "react-router-dom";
-import Btn from "../../components/Btn";
-import TextInput from "../../components/input/TextInput";
 import ProgressBar from "../../components/ProgressBar";
 import SignWrapper from "../../components/SignWrapper";
-
-const round = [
-  {
-    title: "Forgot your password?",
-    btnTitle: "Send Email",
-  },
-  {
-    title: "Reset your password",
-    btnTitle: "Reset password",
-  },
-];
+import ExampleData from "../../ExampleData";
+import { useSignStateData } from "../../hooks/useSignStateData";
+import ForgotPasswordEmail from "./view/ForgotPasswordEmail";
+import ForgotPasswordPassword from "./view/ForgotPasswordPassword";
 
 const ForgotPassword = () => {
+  const { storeDataInState } = useSignStateData();
   const [currentRound, setCurrentRound] = useState(0);
+  const { forgotPasswordRounds } = ExampleData();
+
   const navigate = useNavigate();
 
-  const { control, handleSubmit } = useForm();
-  const onSubmit = (data) => {
-    console.log(data);
-    navigate("/homepage");
+  const handleCallback = ({ data, nextStep }) => {
+    switch (nextStep) {
+      case "google":
+        setCurrentRound((prevState) => prevState + 2);
+        break;
+      case "finished":
+        storeDataInState(data);
+        navigate("/homepage");
+        break;
+      case true:
+        storeDataInState(data);
+        setCurrentRound((prevState) => prevState + 1);
+        break;
+      default:
+        break;
+    }
   };
 
-  const handleSignInClick = () => navigate("/sign-in");
-  const handleSignUpClick = () => navigate("/sign-up");
-
-  const handleContinueClick = () =>
-    currentRound !== round.length - 1 &&
-    setCurrentRound((prevState) => prevState + 1);
-  const handleGoogle = () => {};
-
-  const handleCallbackPrimaryBtn = () =>
-    currentRound === round.length - 1 ? onSubmit : handleContinueClick;
+  const renderComponent = () => {
+    switch (currentRound) {
+      case 0:
+        return <ForgotPasswordEmail handleCallback={handleCallback} />;
+      case 1:
+        return <ForgotPasswordPassword handleCallback={handleCallback} />;
+      default:
+        return "";
+    }
+  };
 
   return (
     <SignWrapper pic="https://images.unsplash.com/photo-1651304285431-e46e5e15ed7d?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=735&q=80">
@@ -44,107 +49,14 @@ const ForgotPassword = () => {
         {/* title part */}
         <div className="flex flex-col items-center justify-center gap-y-1 p-2">
           <span className="text-2xl text-lmGrey800 dark:text-dmGrey25">
-            {round[currentRound].title}
+            {forgotPasswordRounds[currentRound].title}
           </span>
-          <ProgressBar amount={round.length} finished={currentRound} />
+          <ProgressBar
+            amount={forgotPasswordRounds.length}
+            finished={currentRound}
+          />
         </div>
-        {/* main main */}
-        <form
-          onSubmit={handleSubmit(onSubmit)}
-          className="flex w-full max-w-[340px] flex-col gap-y-8"
-        >
-          {/* inputs */}
-          <div className="flex w-full flex-col gap-y-3">
-            <div className={currentRound === 0 ? "flex" : "hidden"}>
-              <Controller
-                name="email"
-                control={control}
-                rules={{
-                  required: "Email is required",
-                  pattern: {
-                    value: /^\w+@[a-zA-Z_]+?\.[a-zA-Z]{2,3}$/,
-                    message: "Invalid email address",
-                  },
-                }}
-                render={({ field, fieldState }) => (
-                  <TextInput
-                    firstIcon="fa-solid fa-at"
-                    onChange={field.onChange}
-                    label="Email"
-                    placeholder="maxMustermann@web.de"
-                    value={field.value}
-                    onBlur={field.onBlur}
-                    error={fieldState.error}
-                  />
-                )}
-              />
-            </div>
-            <div className={currentRound === 1 ? "flex" : "hidden"}>
-              <Controller
-                name="password"
-                control={control}
-                rules={{
-                  required: "Password is required",
-                  pattern: {
-                    value:
-                      /^\S*(?=\S{6,})(?=\S*\d)(?=\S*[A-Z])(?=\S*[a-z])(?=\S*[!@#$%^&*? ])\S*$/,
-                    message:
-                      "Minimum 6 Characters - 1 upper and 1 lower case - 1 letter and 1 special character",
-                  },
-                }}
-                render={({ field, fieldState }) => (
-                  <TextInput
-                    firstIcon="fa-solid fa-lock"
-                    onChange={field.onChange}
-                    label="New password"
-                    placeholder="••••••"
-                    type="password"
-                    value={field.value}
-                    onBlur={field.onBlur}
-                    error={fieldState.error}
-                  />
-                )}
-              />
-            </div>
-          </div>
-          {/* bottom */}
-          <div className="flex flex-col gap-y-2 text-sm">
-            <Btn
-              type="submit"
-              uiType="primary"
-              onClick={handleCallbackPrimaryBtn}
-              title={round[currentRound].btnTitle}
-            />
-            <Btn
-              type="submit"
-              uiType="secondary"
-              onClick={handleGoogle}
-              title="Sign In with Google"
-            />
-            {currentRound === 0 && (
-              <div className="flex flex-col gap-y-1">
-                <span className="text-lmGrey500 dark:text-dmGrey300">
-                  Already have an Account?{" "}
-                  <span
-                    onClick={handleSignInClick}
-                    className="cursor-pointer underline underline-offset-2 duration-300 dark:hover:text-dmGrey100"
-                  >
-                    Sign In instead
-                  </span>
-                </span>
-                <span className="text-lmGrey500 dark:text-dmGrey300">
-                  Don't have a account?{" "}
-                  <span
-                    onClick={handleSignUpClick}
-                    className="cursor-pointer underline underline-offset-2 duration-300 dark:hover:text-dmGrey100"
-                  >
-                    Sign Up instead
-                  </span>
-                </span>
-              </div>
-            )}
-          </div>
-        </form>
+        {renderComponent()}
       </div>
     </SignWrapper>
   );
---
+++ src/pages/forgetPassword/view/ForgotPasswordEmail.jsx
@@ -0,0 +1,69 @@
+import React from "react";
+import { Controller, useForm } from "react-hook-form";
+import { useNavigate } from "react-router-dom";
+import TextInput from "../../../components/input/TextInput";
+import BottomPart from "../../signUp/view/BottomPart";
+
+const ForgotPasswordEmail = ({ handleCallback }) => {
+  const { control, handleSubmit } = useForm();
+  const onSubmit = (data) => {
+    console.log("sending email");
+    const nextStep = true;
+    handleCallback({ data, nextStep });
+  };
+
+  const handleGoogle = () => {
+    console.log("making google stuff");
+    const nextStep = "google";
+    handleCallback({ nextStep });
+  };
+
+  // outsource
+  const navigate = useNavigate();
+  const handleSignInClick = () => navigate("/sign-in");
+
+  return (
+    <form
+      onSubmit={handleSubmit(onSubmit)}
+      className="flex w-full max-w-[340px] flex-col gap-y-8"
+    >
+      <Controller
+        name="email"
+        control={control}
+        rules={{
+          required: "Email is required",
+          pattern: {
+            value: /^\w+@[a-zA-Z_]+?\.[a-zA-Z]{2,3}$/,
+            message: "Invalid email address",
+          },
+        }}
+        render={({ field, fieldState }) => (
+          <TextInput
+            firstIcon="fa-solid fa-at"
+            onChange={field.onChange}
+            label="Email"
+            placeholder="maxMustermann@web.de"
+            value={field.value}
+            onBlur={field.onBlur}
+            error={fieldState.error}
+          />
+        )}
+      />
+      <BottomPart
+        firstBtn="primary"
+        firstBtnTitle="Send Email"
+        firstBtnType="submit"
+        firstBtnOnClick={handleSubmit}
+        secondBtn="secondary"
+        secondBtnTitle="Sign Up with Google"
+        secondBtnType="button"
+        secondBtnOnClick={handleGoogle}
+        underBtnFirstText="Already have an Account?"
+        underBtnFirstLinkText="Sign In instead"
+        underBtnFirstOnClick={handleSignInClick}
+      />
+    </form>
+  );
+};
+
+export default ForgotPasswordEmail;

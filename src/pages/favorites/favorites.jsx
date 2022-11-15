4f3cc74216f895c4ea0a0b52a6831b53e9d37781
Meggle <megglebande@web.de>
2022-11-15 18:10:33 +0100

    kinda favorites, (real) filter logic missing

--- src/pages/favorites/favorites.jsx
+++ src/pages/favorites/favorites.jsx
@@ -1,8 +1,10 @@
 import React from 'react'
+import Catalog from '../catalog/Catalog'
 
 const Favorites = () => {
+  // do the logic in here and than pass the results of the liked (filtered) offers to catalog
   return (
-    <div>favorites</div>
+    <Catalog />
   )
 }
 

5b4c1cee06795d881fdd006da0ed700b5d69e875
Meggle <megglebande@web.de>
2022-11-15 07:47:37 +0100

    multiple modal bug + rest

--- src/layouts/ModalWrapper.jsx
+++ src/layouts/ModalWrapper.jsx
@@ -1,11 +1,7 @@
 import { Dialog, Transition } from "@headlessui/react";
 import React, { Fragment } from "react";
-import ExampleData from "../ExampleData";
 
 const ModalWrapper = ({ isOpen, closeModal, children }) => {
-  const { userProfileBig } = ExampleData();
-  const { firstName, lastName, birthday, email } = userProfileBig;
-
   return (
     <Transition appear show={isOpen} as={Fragment}>
       <Dialog
@@ -23,10 +19,7 @@ const ModalWrapper = ({ isOpen, closeModal, children }) => {
           leaveFrom="opacity-100"
           leaveTo="opacity-0"
         >
-          <div
-            onClick={closeModal}
-            className="fixed inset-0 bg-black bg-opacity-25"
-          />
+          <Dialog.Backdrop className="fixed inset-0 bg-black bg-opacity-25" />
         </Transition.Child>
 
         {/* main */}
@@ -39,43 +32,18 @@ const ModalWrapper = ({ isOpen, closeModal, children }) => {
           leaveFrom="opacity-100 scale-100"
           leaveTo="opacity-0 scale-95"
         >
-          <Dialog.Panel className="scroll fixed top-20 bottom-0 w-full overflow-scroll rounded-t-2xl 820:w-[820px] ">
+          <Dialog.Panel className="fixed top-20 bottom-0 w-full overflow-scroll rounded-t-2xl 820:w-[820px] ">
             {/* Todo: when scrolling then top-10 or so and when the user is at top then top-20 */}
-            <div className="relative flex w-full max-w-[820px] flex-col items-center rounded-t-2xl bg-white p-6 shadow dark:bg-dmGrey900 600:pt-12">
-              <div
+            <div className="relative flex h-fit min-h-full w-full max-w-[820px] flex-col items-center rounded-t-2xl bg-white p-6 shadow dark:bg-dmGrey900 600:pt-12">
+              <button
+                autoFocus={true}
+                className="pointer-events-none absolute opacity-0"
+              ></button>
+              <button
                 onClick={closeModal}
                 className="fa-solid fa-times absolute top-6 right-6 cursor-pointer text-[24px] text-lmGrey700 dark:text-dmGrey25 600:top-10 600:right-12"
               />
               {children}
-              {/* <div className="flex w-full max-w-[472px] flex-col items-center justify-center gap-y-6">
-                <div className="flex flex-col gap-4">
-                  <UserProfileHeader
-                    firstName={firstName}
-                    lastName={lastName}
-                    birthday={birthday}
-                    email={email}
-                  />
-                  <Dialog.Description className="text-sm text-lmGrey600 dark:text-dmGrey100">
-                    I rather take my bike or the OVP to the Uni so I don’t
-                    really have a need for my cars. If you like any of my
-                    offer(s), don’t be scared to send me a Message 😊.
-                  </Dialog.Description>
-                </div>
-
-                <div className="flex w-full flex-col gap-2">
-                  <span className="text-base font-semibold text-lmGrey700 dark:text-dmGrey25">
-                    Offers
-                  </span>
-                  <LittleOfferCardList />
-                </div>
-
-                <div className="flex w-full flex-col gap-2">
-                  <span className="text-base font-semibold text-lmGrey700 dark:text-dmGrey25">
-                    Ratings
-                  </span>
-                  <ReviewSection />
-                </div>
-              </div> */}
             </div>
           </Dialog.Panel>
         </Transition.Child>
---
+++ src/pages/catalog/view/FilterModal.jsx
@@ -0,0 +1,239 @@
+import React from "react";
+import { Controller, useForm } from "react-hook-form";
+import Select from "../../../components/input/Select";
+import TextInput from "../../../components/input/TextInput";
+import ExampleData from "../../../ExampleData";
+import ModalWrapper from "../../../layouts/ModalWrapper";
+
+const FilterModal = ({ isOpen, closeModal, handleFilterCallback }) => {
+  const { control, handleSubmit } = useForm();
+
+  const cleanUpFilterData = (data) => {
+    let allActives = {};
+    Object.entries(data).some((item) => {
+      if (item[1] !== undefined && item[1] !== "") {
+        allActives[item[0]] = item[1];
+      }
+    });
+
+    return allActives;
+  };
+
+  const onSubmit = (data) => {
+    handleFilterCallback(cleanUpFilterData(data));
+  };
+
+  const { filterSelects } = ExampleData();
+  const {
+    transmissionSelect,
+    fuelSelect,
+    seatSelect,
+    trunkSelect,
+    colorSelect,
+    smokingSelect,
+  } = filterSelects;
+
+  // todo: set default values depending on url
+  return (
+    <ModalWrapper isOpen={isOpen} closeModal={closeModal}>
+      <form
+        onSubmit={handleSubmit(onSubmit)}
+        // w-80
+        className="relative flex h-[640px] w-[340px] max-w-[340px] flex-col gap-y-3 overflow-y-scroll rounded-lg"
+      >
+        <div className="flex flex-col gap-y-3">
+          <span className="text-2xl text-lmGrey700 dark:text-dmGrey25">
+            Filter
+          </span>
+
+          <div className="gap-y-1">
+            <div className="flex gap-x-2">
+              <Controller
+                name="startDate"
+                control={control}
+                render={({ field, fieldState }) => (
+                  <TextInput
+                    firstIcon="fa-solid fa-calendar-days"
+                    onChange={field.onChange}
+                    label="Start date"
+                    placeholder="02.06.2023"
+                    type="text"
+                    value={field.value}
+                    onBlur={field.onBlur}
+                    error={fieldState.error}
+                  />
+                )}
+              />
+              <Controller
+                name="endDate"
+                control={control}
+                render={({ field, fieldState }) => (
+                  <TextInput
+                    firstIcon="fa-solid fa-calendar-days"
+                    onChange={field.onChange}
+                    label="End date"
+                    placeholder="21.06.2023"
+                    type="text"
+                    value={field.value}
+                    onBlur={field.onBlur}
+                    error={fieldState.error}
+                  />
+                )}
+              />
+            </div>
+          </div>
+
+          <div className="gap-y-1">
+            <div className="flex gap-x-2">
+              <Controller
+                name="priceStart"
+                control={control}
+                render={({ field, fieldState }) => (
+                  <TextInput
+                    firstIcon="fa-solid fa-coins"
+                    secondIcon="fa-solid fa-euro"
+                    onChange={field.onChange}
+                    label="Start price"
+                    placeholder="30..."
+                    value={field.value}
+                    onBlur={field.onBlur}
+                    error={fieldState.error}
+                  />
+                )}
+                rules={{
+                  pattern: {
+                    value: /^[0-9]+$/,
+                    message: "Only numbers are allowed",
+                  },
+                }}
+              />
+              <Controller
+                name="priceEnd"
+                control={control}
+                render={({ field, fieldState }) => (
+                  <TextInput
+                    firstIcon="fa-solid fa-coins"
+                    secondIcon="fa-solid fa-euro"
+                    onChange={field.onChange}
+                    label="End price"
+                    placeholder="300..."
+                    value={field.value}
+                    onBlur={field.onBlur}
+                    error={fieldState.error}
+                  />
+                )}
+                rules={{
+                  pattern: {
+                    value: /^[0-9]+$/,
+                    message: "Only numbers are allowed",
+                  },
+                }}
+              />
+            </div>
+          </div>
+
+          <div className=" flex flex-col gap-y-1">
+            <span className="text-sm text-lmGrey500 dark:text-dmGrey100">
+              Car Specifics
+            </span>
+            <div className="flex flex-col gap-y-2">
+              <Controller
+                name="transmission"
+                control={control}
+                render={({ field, fieldState }) => (
+                  <Select
+                    icon={transmissionSelect.icon}
+                    placeholder={transmissionSelect.placeholder}
+                    itemList={transmissionSelect.list}
+                    onChange={field.onChange}
+                    label={transmissionSelect.label}
+                    error={fieldState.error}
+                  />
+                )}
+              />
+              <Controller
+                name="fuelType"
+                control={control}
+                render={({ field, fieldState }) => (
+                  <Select
+                    icon={fuelSelect.icon}
+                    placeholder={fuelSelect.placeholder}
+                    itemList={fuelSelect.list}
+                    onChange={field.onChange}
+                    label={fuelSelect.label}
+                    error={fieldState.error}
+                  />
+                )}
+              />
+              <Controller
+                name="seats"
+                control={control}
+                render={({ field, fieldState }) => (
+                  <Select
+                    icon={seatSelect.icon}
+                    placeholder={seatSelect.placeholder}
+                    itemList={seatSelect.list}
+                    onChange={field.onChange}
+                    label={seatSelect.label}
+                    error={fieldState.error}
+                  />
+                )}
+              />
+              <Controller
+                name="trunkVolume"
+                control={control}
+                render={({ field, fieldState }) => (
+                  <Select
+                    icon={trunkSelect.icon}
+                    placeholder={trunkSelect.placeholder}
+                    itemList={trunkSelect.list}
+                    onChange={field.onChange}
+                    label={trunkSelect.label}
+                    error={fieldState.error}
+                  />
+                )}
+              />
+              <Controller
+                name="color"
+                control={control}
+                render={({ field, fieldState }) => (
+                  <Select
+                    icon={colorSelect.icon}
+                    placeholder={colorSelect.placeholder}
+                    itemList={colorSelect.list}
+                    onChange={field.onChange}
+                    label={colorSelect.label}
+                    error={fieldState.error}
+                  />
+                )}
+              />
+              <Controller
+                name="smoking"
+                control={control}
+                render={({ field, fieldState }) => (
+                  <Select
+                    icon={smokingSelect.icon}
+                    placeholder={smokingSelect.placeholder}
+                    itemList={smokingSelect.list}
+                    onChange={field.onChange}
+                    label={smokingSelect.label}
+                    error={fieldState.error}
+                  />
+                )}
+              />
+            </div>
+          </div>
+        </div>
+
+        <button
+          type="submit"
+          className="max-w-[340px] rounded-lg bg-lmPrimary px-3 py-[10px] text-sm font-semibold text-lmGrey25 shadow-md shadow-dmPrimary/40 duration-300 hover:scale-102 active:scale-98 dark:bg-dmPrimary"
+        >
+          Update Filter
+        </button>
+      </form>
+    </ModalWrapper>
+  );
+};
+
+export default FilterModal;

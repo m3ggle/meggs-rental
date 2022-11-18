c14850d2637a240fc62228c73d7a222d57337e03
Meggle <megglebande@web.de>
2022-11-18 10:58:44 +0100

    homepage hero

--- src/pages/catalog/view/CatalogLikeView.jsx
+++ src/pages/catalog/view/CatalogLikeView.jsx
@@ -1,8 +1,23 @@
-import React from "react";
+import React, { useState } from "react";
+import { useEffect } from "react";
+import { useSearchParams } from "react-router-dom";
 import CatalogList from "./CatalogList";
 import LayoutCatalog from "./LayoutCatalog";
 
 const CatalogLikeView = ({offerList}) => {
+  const [filteredOffers, setFilteredOffers] = useState(offerList)
+  let [searchParams, setSearchParams] = useSearchParams();
+
+  useEffect(() => {
+    let tempFilteredOffers = []
+  }, [searchParams]);
+
+  const getAllParams = () => {
+    for (const [key, value] of searchParams.entries()) {
+      console.log(`${key}: ${value}`);
+    }
+  }
+
   return (
     <LayoutCatalog>
       <CatalogList offerList={offerList} />

76ec97bf1e8ff39d19c3814180e06d629b852b89
Meggle <megglebande@web.de>
2022-11-19 16:40:55 +0100

    more profile

--- src/pages/profile/Profile.jsx
+++ src/pages/profile/Profile.jsx
@@ -5,22 +5,13 @@ import ExampleData from "../../ExampleData";
 import ProfileButtonList from "./components/ProfileButtonList";
 import { profileButtonListData } from "./data/profileButtonListData";
 
-const Profile = () => {
-  const { userProfileBig } = ExampleData();
-  const { firstName, lastName, birthday, email } = userProfileBig;
+const { userProfileBig } = ExampleData();
 
+const Profile = () => {
   return (
-    <SignWrapper
-      puffer={false}
-      pic="https://images.unsplash.com/photo-1635107510862-53886e926b74?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2535&q=80"
-    >
+    <SignWrapper puffer={false} pic={userProfileBig.photoUrl}>
       <div className="flex w-full max-w-[348px] flex-col gap-y-3 overflow-y-scroll px-[2px]">
-        <UserProfileHeader
-          firstName={firstName}
-          lastName={lastName}
-          birthday={birthday}
-          email={email}
-        />
+        <UserProfileHeader userProfileData={userProfileBig} />
         <ProfileButtonList profileButtonList={profileButtonListData} />
       </div>
     </SignWrapper>
--- src/pages/profile/subpages/ProfileNotification.jsx
+++ src/pages/profile/subpages/ProfileNotification.jsx
@@ -1,13 +1,13 @@
 import React from "react";
 import SignWrapper from "../../../components/SignWrapper";
+import ExampleData from "../../../ExampleData";
 import ProfileSubPageHeader from "../components/ProfileSubPageHeader";
+
+const { userProfileBig } = ExampleData();
 
 const ProfileNotification = () => {
   return (
-    <SignWrapper
-      puffer={false}
-      pic="https://images.unsplash.com/photo-1658391157361-43b9984cbddf?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80"
-    >
+    <SignWrapper puffer={false} pic={userProfileBig.photoUrl}>
       <div className="flex w-full max-w-[348px] flex-col gap-y-3 overflow-y-scroll px-[2px] py-6">
         <ProfileSubPageHeader title="Notification" />
         <span className="w-full text-center text-lmGrey600 dark:text-dmGrey100">

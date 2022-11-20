14ae47dcdbefa18dcdec8520dc65b3c729a5e7e8
Meggle <megglebande@web.de>
2022-11-20 16:37:31 +0100

    beginning chat

--- src/pages/chat/view/chatSidebar/ChatSidebarMain.jsx
+++ src/pages/chat/view/chatSidebar/ChatSidebarMain.jsx
@@ -1,25 +1,24 @@
 import React from "react";
-import { useEffect } from "react";
 import { useNavigate, useSearchParams } from "react-router-dom";
 import UserProfileChat from "../../../../components/userProfile/UserProfileChat";
 import ExampleData from "../../../../ExampleData";
 import { useWindowSize } from "../../../../hooks/useWindowSize";
 
 const ChatSidebarMain = () => {
-  const { chatPreviews } = ExampleData()
+  const { chatPreviews } = ExampleData();
   let [searchParams, setSearchParams] = useSearchParams();
-  
-  const navigate = useNavigate()
+
+  const navigate = useNavigate();
   const windowSize = useWindowSize();
 
   const handleClick = (chatId) => {
     if (windowSize.width > 1000) {
-      searchParams.set("chatId", chatId)
-      setSearchParams(searchParams)
+      searchParams.set("chatId", chatId);
+      setSearchParams(searchParams);
     } else {
       navigate(`/chat/chat-main/${chatId}`);
     }
-  }
+  };
 
   return (
     <div className="flex w-full flex-col gap-y-2">

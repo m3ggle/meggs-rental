e9d5ceb4f6deb7e1d4be90eb336ac36c544fdfa1
Meggle <megglebande@web.de>
2022-11-20 17:15:33 +0100

    chat

--- src/pages/chat/view/chatSidebar/ChatSidebarMain.jsx
+++ src/pages/chat/view/chatSidebar/ChatSidebarMain.jsx
@@ -1,20 +1,20 @@
 import React from "react";
-import { useNavigate, useSearchParams } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 import UserProfileChat from "../../../../components/userProfile/UserProfileChat";
 import ExampleData from "../../../../ExampleData";
+import { useUrlManipulation } from "../../../../hooks/urlManipulation/useUrlManipulation";
 import { useWindowSize } from "../../../../hooks/useWindowSize";
 
 const ChatSidebarMain = () => {
+  const { setIndividualParam } = useUrlManipulation();
   const { chatPreviews } = ExampleData();
-  let [searchParams, setSearchParams] = useSearchParams();
 
   const navigate = useNavigate();
   const windowSize = useWindowSize();
 
   const handleClick = (chatId) => {
     if (windowSize.width > 1000) {
-      searchParams.set("chatId", chatId);
-      setSearchParams(searchParams);
+      setIndividualParam("chatId", chatId);
     } else {
       navigate(`/chat/chat-main/${chatId}`);
     }

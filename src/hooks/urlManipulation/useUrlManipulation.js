78c086270185207d2000a2d8ea87efc0db3663a4
Meggle <megglebande@web.de>
2022-11-20 18:56:22 +0100

    filter

--- src/hooks/urlManipulation/useUrlManipulation.js
+++ src/hooks/urlManipulation/useUrlManipulation.js
@@ -1,12 +1,30 @@
 import { useSearchParams } from "react-router-dom";
 
 export const useUrlManipulation = () => {
-    let [searchParams, setSearchParams] = useSearchParams();
+  let [searchParams, setSearchParams] = useSearchParams();
 
-    const setIndividualParam = (desiredParam, desiredValue) => {
-        searchParams.set(desiredParam, desiredValue)
-        setSearchParams(searchParams)
-    }
-    
-    return { setIndividualParam };
-}
+  // setting
+  const setIndividualParam = (desiredParam, desiredValue) => {
+    searchParams.set(desiredParam, desiredValue);
+    setSearchParams(searchParams);
+  };
+  const setArrayOfParams = () => {};
+
+  // getting
+  const getIndividualParam = (desiredParam) => {
+    return searchParams.get(desiredParam);
+  };
+  const getArrayOfParams = () => {};
+
+  // delete
+  const deleteIndividualParam = (desiredParam) => {
+    searchParams.delete(desiredParam);
+    setSearchParams(searchParams);
+  };
+  const deleteAllParams = () => {};
+  const deleteArrayOfParams = () => {};
+
+  // handle (more compplex)
+
+  return { setIndividualParam };
+};

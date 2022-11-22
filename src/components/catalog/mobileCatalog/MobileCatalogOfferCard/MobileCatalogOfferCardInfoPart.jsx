37a6de74f038ccc88972baac712d9a9d76bca788
Meggle <megglebande@web.de>
2022-11-22 21:15:50 +0100

    preparation for map functionality

--- src/components/catalog/mobileCatalog/MobileCatalogOfferCard/MobileCatalogOfferCardInfoPart.jsx
+++ src/components/catalog/mobileCatalog/MobileCatalogOfferCard/MobileCatalogOfferCardInfoPart.jsx
@@ -8,32 +8,29 @@ const MobileCatalogOfferCardInfoPart = ({onNavigationCallback, offerInformation 
       onClick={onNavigationCallback}
       className="flex w-full flex-col gap-y-1 overflow-hidden rounded-xl py-3 dark:bg-dmGrey900"
     >
-      {/* name and location */}
       <div className="flex flex-col gap-y-1">
-        {/* location */}
-        <div className="flex w-5/6 items-center gap-x-1 text-xs text-lmGrey600 dark:text-dmGrey100 500:w-fit">
+        <div className="flex w-9/12 items-center gap-x-1 text-xs text-lmGrey600 dark:text-dmGrey100">
           <i className="fa-solid fa-location-dot"></i>
-          <span>{location.formatted}</span>
+          <span className="w-full truncate">{location.formatted}</span>
         </div>
-        {/* name */}
-        <span className="w-5/6 text-base font-semibold text-lmGrey700 dark:text-dmGrey25 500:w-fit">
+        <span className="truncate text-base  w-9/12 font-semibold text-lmGrey700 dark:text-dmGrey25">
           {name}
         </span>
       </div>
-      {/* price */}
-      <div className="flex items-center gap-x-[2px] truncate">
-        <span className="text-lg text-lmPrimary dark:text-dmPrimary">
+
+      <div className="flex w-full items-center gap-x-[2px] truncate">
+        <span className="text-base text-lmPrimary dark:text-dmPrimary">
           {price.day}€{" "}
           <span className="text-sm text-lmGrey400 dark:text-dmGrey300">
             /day
           </span>
         </span>
         <div className="flex items-center justify-center px-1">
-          <div className="h-1 w-1 rounded-full bg-lmGrey400"></div>
+          <div className="min-h-1 min-w-1 rounded-full bg-lmGrey400"></div>
         </div>
-        <span className="text-lg text-lmPrimary dark:text-dmPrimary">
+        <span className="flex items-center truncate text-base text-lmPrimary dark:text-dmPrimary">
           {price.week}€{" "}
-          <span className="text-sm text-lmGrey400 dark:text-dmGrey300">
+          <span className="truncate text-sm text-lmGrey400 dark:text-dmGrey300">
             /week
           </span>
         </span>
--- src/components/preview/components/PreviewBasicInfo.jsx
+++ src/components/preview/components/PreviewBasicInfo.jsx
@@ -3,16 +3,21 @@ import React from 'react'
 const PreviewBasicInfo = () => {
   return (
     <div className="flex flex-col gap-y-1">
-      <div className="flex items-center text-lmGrey600">
+      <div className="flex items-center text-lmGrey600 dark:text-dmGrey100">
         <div
           className="fa-solid fa-location-dot mb-[3px] h-[14px] w-[14px] text-[14px]"
           aria-hidden="true"
         />
-        <span className="text-sm cursor-pointer">Salzbuger Straße 18</span>
+        <span className="cursor-pointer text-sm">Salzbuger Straße 18</span>
       </div>
-      <span className="text-xl text-lmGrey800">BMW M3 E30</span>
-      <span className="text-lg text-lmPrimary">
-        600€ <span className="text-sm text-lmGrey400">/month</span>
+      <span className="text-xl text-lmGrey800 dark:text-dmGrey25">
+        BMW M3 E30
+      </span>
+      <span className="dark:text-dmGreyPrimary text-lg text-lmPrimary">
+        600€{" "}
+        <span className="text-sm text-lmGrey400 dark:text-dmGrey300">
+          /month
+        </span>
       </span>
     </div>
   );

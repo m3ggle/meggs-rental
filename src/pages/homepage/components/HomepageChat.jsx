84281f8d89836df7f3d1be127a0da76838b8f3c8
Meggle <megglebande@web.de>
2022-12-10 14:39:38 +0100

    Homepage Explore and Chat #191

--- src/pages/homepage/components/HomepageChat.jsx
+++ src/pages/homepage/components/HomepageChat.jsx
@@ -1,11 +1,18 @@
 import React from "react";
 import Btn from "../../../components/common/Btn";
+import MobileChatSidebar from "../../../assets/img/mobileChatSidebar.webp"
+import DesktopChat from "../../../assets/img/desktopChat.webp";
+import TableExploreMap from "../../../assets/img/tabletExploreMap.webp";
+import { useNavigate } from "react-router-dom";
+
 
 const HomepageChat = () => {
+  const navigate = useNavigate()
+
   return (
     <div
       id="chat"
-      className="flex w-full flex-col gap-y-6 bg-white px-14 py-[100px]"
+      className="relative flex w-full flex-col gap-y-6 bg-white px-14 py-[100px]"
     >
       <div className="flex w-[615px] flex-col gap-y-6">
         <span className="text-5xl font-semibold text-lmGrey800 drop-shadow">
@@ -18,7 +25,12 @@ const HomepageChat = () => {
         </span>
         <div className="flex w-full gap-x-3">
           <div className="h-fit">
-            <Btn title="Find Offer" uiType="secondary" type="button" />
+            <Btn
+              title="Find Offer"
+              uiType="secondary"
+              type="button"
+              onClick={() => navigate("/explore/catalog")}
+            />
           </div>
           <div className="h-fit">
             <Btn
@@ -26,20 +38,37 @@ const HomepageChat = () => {
               uiType="primary"
               type="button"
               icon="fa-solid fa-chevron-right"
+              onClick={() => navigate("/chat")}
             />
           </div>
         </div>
       </div>
-      <div className="overflow relative flex h-[571px] w-full max-w-full items-baseline bg-pink-100 px-1">
-        <div className="absolute left-0 bottom-0 z-20 min-h-[404px] min-w-[200px] bg-blue-200"></div>
-        <div className="absolute left-[100px] bottom-0 z-10 min-h-[574.4px] min-w-[1000px] bg-blue-300"></div>
-        <div className="absolute right-0 bottom-0 min-h-[831.84px] min-w-[600px] bg-blue-400"></div>
-      </div>
-      {/* <div className="relative flex h-[571px] bg-pink-100 items-baseline w-full max-w-full">
-        <img className="h-fit w-[200px] z-20" src={MobileChatSidebar} alt="tablet" />
-        <img className="-ml-[100px] h-fit w-[1000px] z-10" src={DesktopChat} alt="desktop" />
-        <img className="h-fit w-[600px]" src={TableExploreMap} alt="desktop" />
-      </div> */}
+
+      <img
+        aria-hidden={true}
+        className="z-10 -ml-[100px] h-fit w-[1000px] opacity-0"
+        src={DesktopChat}
+        alt="desktop"
+      />
+
+      <img
+        onClick={() => navigate("/chat")}
+        className="absolute left-14 bottom-[100px] z-20 h-fit w-[14%] cursor-pointer drop-shadow-lg duration-300 hover:scale-101 active:scale-99"
+        src={MobileChatSidebar}
+        alt="tablet"
+      />
+      <img
+        onClick={() => navigate("/chat")}
+        className="absolute left-[156px] bottom-[100px] z-10 -ml-[100px] h-fit w-[69.44%] cursor-pointer drop-shadow-lg duration-300 hover:scale-101 active:scale-99"
+        src={DesktopChat}
+        alt="desktop"
+      />
+      <img
+        onClick={() => navigate("/explore/map")}
+        className="absolute right-14 bottom-[100px] h-fit w-[41.66%] cursor-pointer drop-shadow-lg duration-300 hover:scale-101 active:scale-99"
+        src={TableExploreMap}
+        alt="desktop"
+      />
     </div>
   );
 };

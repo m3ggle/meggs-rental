92661cdc55263c7d66e69dd988ae84f4a9066037
Meggle <megglebande@web.de>
2022-12-13 09:14:14 +0100

    most viewed

--- src/pages/homepage/components/HomepageMostViewed.jsx
+++ src/pages/homepage/components/HomepageMostViewed.jsx
@@ -11,63 +11,80 @@ const { exampleOffers } = ExampleData();
 
 const HomepageMostViewed = () => {
   const week = exampleOffers[3];
-    const month = exampleOffers[0];
-    const favorite = exampleOffers[1]
+  const month = exampleOffers[0];
+  const favorite = exampleOffers[1];
   const today = [exampleOffers[4], exampleOffers[5], exampleOffers[6]];
 
   return (
     <div
       id="mostViewed"
-      className="flex w-full flex-col items-center gap-y-6 bg-white py-[100px]"
+      className="hideScrollbar flex w-full flex-col items-center gap-x-6 overflow-x-scroll bg-white py-16 700:py-[100px] 1200:gap-y-6"
     >
-      <div className="flex items-end gap-x-6">
-        <div className="flex flex-col gap-y-3 rounded-[24px] p-9 shadow">
-          <h3 className="text-3xl text-lmGrey800 drop-shadow-sm">This Month</h3>
-          <TabletOfferCard offerInformation={week} index={0} />
-        </div>
-
-        <div className="flex flex-col gap-y-3 rounded-[24px] bg-white p-14 shadow">
-          <h3 className="text-4xl text-lmGrey800">This Month</h3>
-          <SpecialHomepageOfferCard offerInformation={month} />
-        </div>
-
-        <div className="flex w-fit flex-col justify-center gap-y-6 p-12">
-          <h2 className="w-full text-5xl text-lmGrey800 drop-shadow">
-            Most Viewed
-          </h2>
-          <span className="w-[297px] text-xl text-lmGrey800 drop-shadow-sm">
-            Here are the best offers for you in different timespans. If you want
-            to see more then open the full page
-          </span>
-          <div className="w-fit">
-            <Btn uiType="primary" type="button" title="Open Full Page" />
-          </div>
+      <div className="flex w-full flex-col gap-y-6 px-6 700:hidden">
+        <h2 className="text-4xl font-semibold -tracking-[1.2%] text-lmGrey800 drop-shadow 700:text-[40px] 700:leading-[40px] 1200:text-5xl">
+          Most Viewed
+        </h2>
+        <span className="w-[297px] text-lg text-lmGrey800 drop-shadow-sm 700:text-xl 1200:text-2xl">
+          Here are the best offers for you in different timespans. If you want
+          to see more then open the full page
+        </span>
+        <div className="w-fit">
+          <Btn uiType="primary" type="button" title="Open Full Page" />
         </div>
       </div>
 
-      <div className="flex items-start gap-x-6">
-        <div className="relative h-[457px] w-[312px] rounded-[24px] shadow">
-          <img
-            className="absolute bottom-0 right-0 h-[335px] min-w-fit"
-            src={Chameleon}
-            alt="Chameleon"
-          />
-        </div>
+      <div className="hideScrollbar flex w-full items-center gap-x-6 overflow-x-scroll py-1 1200:py-0 1200:flex-col 1200:gap-y-6">
+        <div className="flex flex-row-reverse gap-x-6 1200:flex-row 1200:items-end">
+          <div className="flex h-fit flex-col  gap-y-3 rounded-[24px] p-9 shadow">
+            <h3 className="text-3xl text-lmGrey800 drop-shadow-sm">
+              This Week
+            </h3>
+            <TabletOfferCard offerInformation={week} index={0} />
+          </div>
 
-        <div className="flex flex-col gap-y-3 rounded-[24px] p-9 shadow">
-          <h3 className="text-3xl text-lmGrey800 drop-shadow-sm">Today</h3>
-          <div className="flex w-full flex-col gap-y-3">
-            {today.map((offer) => (
-              <MobileOfferCard key={offer.offerId} offerInformation={offer} />
-            ))}
+          <div className="flex flex-col gap-y-3 rounded-[24px] bg-white p-14 shadow">
+            <h3 className="text-4xl text-lmGrey800">This Month</h3>
+            <SpecialHomepageOfferCard offerInformation={month} />
+          </div>
+
+          <div className="hidden w-fit flex-col gap-y-6 p-12 700:flex 1200:justify-center">
+            <h2 className="text-4xl font-semibold -tracking-[1.2%] text-lmGrey800 drop-shadow 700:text-[40px] 700:leading-[40px] 1200:text-5xl">
+              Most Viewed
+            </h2>
+            <span className="w-[297px] text-lg text-lmGrey800 drop-shadow-sm 700:text-xl 1200:text-2xl">
+              Here are the best offers for you in different timespans. If you
+              want to see more then open the full page
+            </span>
+            <div className="w-fit">
+              <Btn uiType="primary" type="button" title="Open Full Page" />
+            </div>
           </div>
         </div>
 
-        <div className="flex flex-col gap-y-3 rounded-[24px] bg-white p-14 shadow">
-          <h3 className="text-4xl text-lmGrey800 drop-shadow-sm">
-            My Favorite
-          </h3>
-          <DesktopOfferCard offerInformation={favorite} />
+        <div className="flex items-start gap-x-6">
+          <div className="relative hidden h-[457px] w-[312px] rounded-[24px] shadow 1200:flex">
+            <img
+              className="absolute bottom-0 right-0 h-[335px] min-w-fit"
+              src={Chameleon}
+              alt="Chameleon"
+            />
+          </div>
+
+          <div className="flex flex-col gap-y-3 rounded-[24px] p-9 shadow">
+            <h3 className="text-3xl text-lmGrey800 drop-shadow-sm">Today</h3>
+            <div className="flex w-full flex-col gap-y-3">
+              {today.map((offer) => (
+                <MobileOfferCard key={offer.offerId} offerInformation={offer} />
+              ))}
+            </div>
+          </div>
+
+          <div className="flex flex-col gap-y-3 rounded-[24px] bg-white p-14 shadow">
+            <h3 className="text-4xl text-lmGrey800 drop-shadow-sm">
+              My Favorite
+            </h3>
+            <DesktopOfferCard offerInformation={favorite} />
+          </div>
         </div>
       </div>
     </div>

386b859b55a64d0910ad93372ed3e926a24f9126
Meggle <megglebande@web.de>
2022-12-13 12:41:10 +0100

    dm homepageNavbar

--- src/pages/homepage/components/HomepageNavbar.jsx
+++ src/pages/homepage/components/HomepageNavbar.jsx
@@ -22,7 +22,7 @@ const HomepageNavbar = ({ openModal }) => {
   };
 
   const textInTheFooter =
-    "text-lmGrey600 cursor-pointer w-fit hover:text-lmGrey800 active:text-lmPrimary duration-300";
+    "text-lmGrey600 dark:text-dmGrey100 cursor-pointer w-fit hover:text-lmGrey800 hover:text-dmGrey25 active:text-lmPrimary dark:active:text-dmPrimary duration-300";
 
   const navbarLinks = [
     {
@@ -48,13 +48,13 @@ const HomepageNavbar = ({ openModal }) => {
   ];
 
   return (
-    <div className="fixed -top-1 left-auto z-40 flex h-fit w-full max-w-[1440px] items-center justify-between border-b-2 border-lmGrey100/50 bg-white/80 py-4 px-6 backdrop-blur-lg dark:bg-dmGrey900/20 700:px-11 1200:px-14">
+    <div className="fixed -top-1 left-auto z-40 flex h-fit w-full max-w-[1440px] items-center justify-between border-b-2 border-lmGrey100/50 bg-white/80 py-4 px-6 backdrop-blur-lg dark:border-dmGrey800/10 dark:bg-dmGrey900/20 700:px-11 1200:px-14">
       {/* logo */}
-      <div className="flex w-fit 700:w-60 cursor-pointer items-center gap-x-1">
+      <div className="flex w-fit cursor-pointer items-center gap-x-1 700:w-60">
         <div
           onClick={openModal}
           className={`h-14 w-14 rounded-full ${
-            !loadedLogo && "bg-lmPrimary"
+            !loadedLogo && "bg-lmPrimary dark:bg-dmPrimary"
           } duration-300`}
         >
           <Spline
@@ -65,14 +65,14 @@ const HomepageNavbar = ({ openModal }) => {
         </div>
         <span
           onClick={() => smoothScroll("hero")}
-          className="text-lg text-lmGrey800"
+          className="text-lg text-lmGrey800 dark:text-dmGrey25"
         >
           Meggs Rental
         </span>
       </div>
 
       {/* links */}
-      <div className="hidden gap-x-6 text-lg text-lmGrey800 1100:flex">
+      <div className="hidden gap-x-6 text-lg text-lmGrey800 dark:text-dmGrey25 1100:flex">
         {navbarLinks.map((navLink, index) => (
           <span
             key={index}
@@ -88,11 +88,11 @@ const HomepageNavbar = ({ openModal }) => {
       <div className="flex w-fit items-center justify-end gap-x-2 700:w-60">
         {signedIn ? (
           <>
-            <span className="text-lg text-lmGrey800">
+            <span className="text-lg text-lmGrey800 dark:text-dmGrey25">
               {firstName} {lastName}
             </span>
             <img
-              className="h-11 w-11 rounded-full bg-blue-200"
+              className="h-11 w-11 rounded-full bg-blue-200 dark:bg-blue-800"
               src={photoUrl}
               alt="user profile"
             />

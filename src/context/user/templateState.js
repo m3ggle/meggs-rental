17a48fbf9b3c9835c1a1ce7db181a6acff3affa1
Meggle <megglebande@web.de>
2022-12-29 15:37:41 +0100

    little bugs

--- src/context/user/templateState.js
+++ src/context/user/templateState.js
@@ -13,7 +13,7 @@ export const templateState = {
     lastViewedOffers: [],
     ownOffers: [],
     phoneNumber: null,
-    photoUrl: null,
+    photoURL: null,
     preferredCity: {
       bounds: {
         east: null,
--- src/pages/homepage/Homepage.jsx
+++ src/pages/homepage/Homepage.jsx
@@ -1,6 +1,4 @@
-
 import React from "react";
-import { useUserContext } from "../../context/user/userContext";
 import HomepageExplore from "./components/explore/HomepageExplore";
 import HomepageFooter from "./components/footer/HomepageFooter";
 import HomepageAbout from "./components/HomepageAbout";
@@ -10,9 +8,6 @@ import HomepageMostViewed from "./components/HomepageMostViewed";
 import HomepageNavbar from "./components/navbar/HomepageNavbar";
 
 const Homepage = () => {
-  const {signedIn} = useUserContext()
-  console.log(signedIn);
-
   return (
     <div className="relative flex w-full max-w-[1440px] flex-col overflow-hidden">
       <HomepageNavbar />

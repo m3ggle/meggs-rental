bc71d6addb853ecfc83e57c1383366edb6bca151
Meggle <megglebande@web.de>
2022-12-29 13:57:06 +0100

    dark mode for notify modal

--- src/components/notifyModal/components/ContentStandard.jsx
+++ src/components/notifyModal/components/ContentStandard.jsx
@@ -7,7 +7,8 @@ const ContentStandard = () => {
   const { title, bulletPoints, primaryButton, secondaryButton } = extraInfo;
 
   return (
-    <div className="hideScrollbar flex h-full w-full flex-col gap-y-9 overflow-scroll 600:gap-y-12 1000:max-w-[340px]">
+    <div className="hideScrollbar flex h-full w-full flex-col gap-y-9 600:gap-y-12 1000:max-w-[340px]">
+    {/* <div className="hideScrollbar flex h-full w-full flex-col gap-y-9 overflow-scroll 600:gap-y-12 1000:max-w-[340px]"> */}
       <div className="flex w-full flex-col gap-y-2 600:gap-y-4">
         <h2 className="text-2xl font-semibold text-lmGrey800 600:text-4xl">
           {title}

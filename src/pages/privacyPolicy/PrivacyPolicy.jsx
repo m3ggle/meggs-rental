5dba35e00b572fe5dbd197c01d1b4d0a779e5190
Meggle <megglebande@web.de>
2023-01-06 14:37:47 +0100

    test react query

--- src/pages/privacyPolicy/PrivacyPolicy.jsx
+++ src/pages/privacyPolicy/PrivacyPolicy.jsx
@@ -1,31 +1,8 @@
 import React from "react";
 import Btn from "../../components/common/Btn";
-import { toastNotify } from "../../components/toastNotify/toastNotify";
 
 const PrivacyPolicy = () => {
-  const { notifyMessage, notifyStandard } = toastNotify();
-
-  const exampleMsg = {
-    from: "Meggle Bande",
-    content: "Hey, how about a new deal on Saturday till Monday",
-    navigateTo: "/chat",
-  };
-
-  const exampleStandard = {
-    type: "info",
-    content: "Offer was uploaded"
-  }
-
-  const handleClick = async () => {
-    notifyStandard({ information: exampleStandard, id: "standard" });
-  };
-
-  const handleClick2 = () => {
-    notifyMessage({
-      information: exampleMsg,
-      id: "sec",
-    });
-  };
+  const handleClick = async () => {};
 
   return (
     <div className="relative flex h-screen w-full flex-col items-center justify-center gap-y-2">
@@ -35,14 +12,6 @@ const PrivacyPolicy = () => {
           uiType="primary"
           type="button"
           onClick={handleClick}
-        />
-      </div>
-      <div className="h-fit w-fit">
-        <Btn
-          title="click to open"
-          uiType="secondary"
-          type="button"
-          onClick={handleClick2}
         />
       </div>
     </div>

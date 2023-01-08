aa799484fc7d61b692ab91152fdbb0944e437168
Meggle <megglebande@web.de>
2023-01-08 09:46:44 +0100

    init supabase

--- src/pages/privacyPolicy/PrivacyPolicy.jsx
+++ src/pages/privacyPolicy/PrivacyPolicy.jsx
@@ -1,28 +1,12 @@
 import { collection, getDocs, limit, query, where } from "firebase/firestore";
 import React from "react";
 import Btn from "../../components/common/Btn";
+import supabase from "../../config/supabaseClient";
 import { db } from "../../firebase.config";
 
 const PrivacyPolicy = () => {
   const handleClick = async () => {
-    const q = query(
-      collection(db, "offers"),
-      where(
-        "price.week",
-        "<=",
-        300,
-        "&&",
-        "name",
-        "!=",
-        "(Test) Lamborghini Countach"
-      ),
-      limit(5)
-    );
-
-    const querySnapshot = await getDocs(q);
-    querySnapshot.forEach((doc) => {
-      console.log(doc.data().name, doc.data().price)
-    });
+    console.log(supabase);
   };
 
   return (
@@ -37,27 +21,6 @@ const PrivacyPolicy = () => {
       </div>
     </div>
   );
-
-  // Example
-  // const { isLoading, error, data } = useQuery("repoData", () =>
-  //   fetch("https://api.github.com/repos/tannerlinsley/react-query").then(
-  //     (res) => res.json()
-  //   )
-  // );
-
-  // if (isLoading) return "Loading...";
-
-  // if (error) return "An error has occurred: " + error.message;
-
-  // return (
-  //   <div>
-  //     <h1>{data.name}</h1>
-  //     <p>{data.description}</p>
-  //     <strong>👀 {data.subscribers_count}</strong>{" "}
-  //     <strong>✨ {data.stargazers_count}</strong>{" "}
-  //     <strong>🍴 {data.forks_count}</strong>
-  //   </div>
-  // );
 };
 
 export default PrivacyPolicy;

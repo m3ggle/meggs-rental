6eb2d55744ff8b50e1372961c0d838753017e9a0
Meggle <megglebande@web.de>
2023-01-08 08:59:29 +0100

    beginning connect catalog to firebase

--- src/pages/explore/catalog/Catalog.jsx
+++ src/pages/explore/catalog/Catalog.jsx
@@ -1,10 +1,14 @@
 import React from "react";
+import { useQuery } from "react-query";
 import CatalogWrapper from "../../../components/wrapper/CatalogWrapper";
+import { useUserContext } from "../../../context/user/userContext";
 import ExampleData from "../../../ExampleData";
 
 const Catalog = () => {
   const { exampleOffers } = ExampleData();
-  return <CatalogWrapper offerList={exampleOffers} />;
+  const { userData } = useUserContext();
+
+  return <CatalogWrapper offerList={exampleOffers} preferredCity={userData?.preferredCity} />;
 };
 
 export default Catalog;

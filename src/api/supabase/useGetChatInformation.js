460b13e7d0b53127119ca87c8290d46225602d07
Meggle <megglebande@web.de>
2023-02-17 19:01:20 +0100

    new useGetChatInformation

---
+++ src/api/supabase/useGetChatInformation.js
@@ -0,0 +1,26 @@
+import { useQuery } from "react-query";
+import supabase from "../../config/supabaseClient";
+
+export const useGetChatInformation = (chatId) => {
+  const getOfferCard = async () => {
+      if (chatId !== null) {
+        console.log("sending to supabase")
+      return await supabase.rpc("get_chat_information", {
+        chat_id: chatId,
+      });
+    }
+    return { error: null, data: null };
+  };
+
+  const { data, isLoading } = useQuery(
+    ["get_chat_information", chatId],
+    getOfferCard,
+    {
+      refetchOnMount: true,
+      refetchOnWindowFocus: false,
+      staleTime: Infinity,
+    }
+  );
+
+  return { chatInformation: data?.data, isLoading, error: data?.error };
+};

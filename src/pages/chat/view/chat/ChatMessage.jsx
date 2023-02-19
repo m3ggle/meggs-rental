6fb571e3f381128730fa645ac58c4d3ce1ef1778
Meggle <megglebande@web.de>
2023-02-19 12:04:25 +0100

    read and unread replaced with monkey

--- src/pages/chat/view/chat/ChatMessage.jsx
+++ src/pages/chat/view/chat/ChatMessage.jsx
@@ -1,24 +1,35 @@
 import React from "react";
 import styles from "../../../../style";
 
-const ChatMessage = ({ isOwner = false, text = "", isFirst = false, isRead }) => {
+const ChatMessage = ({
+  isOwner = false,
+  text = "",
+  isFirst = false,
+  isRead,
+}) => {
   return (
     <div
       className={`${
         isOwner ? "justify-end" : "justify-start"
       } flex h-fit w-full`}
     >
-      <div className="flex h-fit w-fit flex-col gap-y-2">
-        <div
-          className={`${
-            isOwner
-              ? "bg-lmPrimary text-white dark:bg-dmPrimary"
-              : `${styles.darkModeBorder} dark: bg-lmGrey25 text-lmGrey600 dark:bg-dmGrey900 dark:text-dmGrey100`
-          } w-fit max-w-[90%] rounded-lg p-4 text-sm 600:max-w-[640px]`}
-        >
-          {text}
-        </div>
-        {isFirst && <span className={`text-lmGrey300 text-sm ${isOwner ? "text-right pr-2" : "text-left pl-2"}`}>{isRead ? "Read " : "Unread"}</span>}
+      <div
+        className={`${
+          isOwner
+            ? "bg-lmPrimary text-white dark:bg-dmPrimary"
+            : `${styles.darkModeBorder} dark: bg-lmGrey25 text-lmGrey600 dark:bg-dmGrey900 dark:text-dmGrey100`
+        } relative w-fit max-w-[90%] rounded-lg p-4 text-sm 600:max-w-[640px]`}
+      >
+        {text}
+        {isFirst && (
+          <span
+            className={`absolute -bottom-1 flex h-[18px] w-[18px] items-center justify-center overflow-visible rounded-full text-lg ${
+              isOwner ? "-left-1" : "-right-2"
+            }`}
+          >
+            {isRead ? "🐵" : "🙈"}
+          </span>
+        )}
       </div>
     </div>
   );

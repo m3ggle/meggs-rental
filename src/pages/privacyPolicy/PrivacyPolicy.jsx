1aa8c5ed02d2580b6b4fb62bc9bd26ded5c39a68
Meggle <megglebande@web.de>
2023-02-22 16:45:54 +0100

    cleanup and homepage hero offercards coming from supabase

--- src/pages/privacyPolicy/PrivacyPolicy.jsx
+++ src/pages/privacyPolicy/PrivacyPolicy.jsx
@@ -1,26 +1,8 @@
 import React from "react";
 import Btn from "../../components/common/Btn";
-import supabase from "../../config/supabaseClient";
 
 const PrivacyPolicy = () => {
-  const handleClick = async () => {
-    const testDrive =
-      "https://cymyxcckynyeemdvnckd.supabase.co/storage/v1/object/public/user-avatars/921ed632-ab15-4812-ad30-b0d258771532.webp";
-    
-    if (testDrive.includes('user-default')) {
-      console.log("default image")
-      return 
-    }
-    console.log(
-      "ready to be deleted",
-      testDrive.split(
-        "https://cymyxcckynyeemdvnckd.supabase.co/storage/v1/object/public/user-avatars/"
-      )
-    );
-    
-    // const cDate = new Date()
-    // console.log(new Date().toISOString());
-  };
+  const handleClick = async () => {};
 
   return (
     <div className="flex h-screen w-full flex-col items-center justify-center gap-y-2 overflow-scroll">

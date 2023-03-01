2ff4fcce76fcb9680d9c5c142a789ee36af56ffe
Meggle <megglebande@web.de>
2023-03-01 18:41:31 +0100

    heart button

--- src/pages/offerDetails/view/OfferDetailsHeader.jsx
+++ src/pages/offerDetails/view/OfferDetailsHeader.jsx
@@ -3,7 +3,6 @@ import { useHandleOfferLikeIcon } from "../../../components/offerCard/hooks/useH
 import { useUserContext } from "../../../context/user/userContext";
 import { copyUrlToClipboard } from "../../../helpers/copyUrlToClipboard";
 import { getCurrentUrl } from "../../../helpers/getCurrentUrl";
-import styles from "../../../style";
 import { handleLikeSetQuery } from "../helpers/handleLikeSetQuery";
 import OfferDetailsHeaderPart from "./OfferDetailsHeaderPart";
 
@@ -25,19 +24,6 @@ const OfferDetailsHeader = ({ offerInformation }) => {
     });
   };
 
-  // debounce
-  const handleLike = async () => {
-    await handleOfferLikeIcon();
-  };
-
-  useEffect(() => {
-    handleLikeSetQuery({
-      offerId: offer_basics.id,
-      userId,
-      isLiked,
-    });
-  }, [isLiked]);
-
   return (
     <div
       className={`flex h-full w-full items-center justify-between gap-y-1 rounded-3xl bg-white p-6 shadow dark:bg-dmGrey900 dark:shadow-dmShadow`}
@@ -47,6 +33,7 @@ const OfferDetailsHeader = ({ offerInformation }) => {
         leftIcon="fa-solid fa-chevron-left"
         value="Go Back"
         onClickCallback={handleGoBack}
+        cursor="cursor-pointer"
       />
       {/* views, likes and share */}
       <div className="flex items-center gap-x-8">
@@ -54,28 +41,22 @@ const OfferDetailsHeader = ({ offerInformation }) => {
         <OfferDetailsHeaderPart
           rightIcon="fa-solid fa-eye"
           value={view_count}
+          hoverEnabled={false}
         />
 
         {/* likes */}
-        <div
-          onClick={handleLike}
-          className={`flex items-center gap-x-2 py-2 ${
-            isLiked
-              ? styles.badgeRedTextGradient
-              : "text-lmGrey600 hover:text-lmGrey800 dark:text-dmGrey100 dark:hover:text-lmGrey25"
-          } `}
-        >
-          <span className="text-base">{like_count}</span>
-          <div className="flex h-[15px] w-[15px] items-center justify-center">
-            <i className="fa-solid fa-heart text-[15px]" />
-          </div>
-        </div>
+        <OfferDetailsHeaderPart
+          rightIcon="fa-solid fa-heart"
+          value={like_count}
+          hoverEnabled={false}
+        />
 
         {/* share */}
         <OfferDetailsHeaderPart
           rightIcon="fa-solid fa-share"
           value="Share"
           onClickCallback={handleShare}
+          cursor="cursor-pointer"
         />
       </div>
     </div>

c348cf56c16efb76b0e54ddb5e76a25b5aa9a3a4
Meggle <megglebande@web.de>
2023-03-03 14:42:27 +0100

    smaller homepage navbar

--- src/pages/homepage/components/navbar/HomepageNavbar.jsx
+++ src/pages/homepage/components/navbar/HomepageNavbar.jsx
@@ -5,7 +5,7 @@ import HomepageNavbarRight from "./HomepageNavbarRight";
 
 const HomepageNavbar = () => {
   return (
-    <div className="fixed -top-1 left-auto z-40 flex h-fit w-full max-w-[1440px] items-center justify-between border-b-2 border-lmGrey100/50 bg-white/80 py-4 px-6 backdrop-blur-lg dark:border-dmGrey800/10 dark:bg-dmGrey900/80 700:px-11 1200:px-14">
+    <div className="fixed -top-1 left-auto z-40 flex h-fit w-full max-w-[1440px] items-center justify-between border-b-2 border-lmGrey100/50 bg-white/80 py-3 px-6 backdrop-blur-lg dark:border-dmGrey800/10 dark:bg-dmGrey900/80 700:px-11 1200:px-14">
       {/* logo */}
       <HomepageNavbarLeft />
 

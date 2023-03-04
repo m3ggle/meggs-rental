bb7a9cebe2ff020a6ac4a4c9c86c12301fb2f3a8
Meggle <megglebande@web.de>
2023-03-04 12:38:39 +0100

    grid try

--- src/components/layouts/CatalogLayout.jsx
+++ src/components/layouts/CatalogLayout.jsx
@@ -7,10 +7,11 @@ const CatalogLayout = ({ children }) => {
   return (
     <div className="flex w-full max-w-[1440px] flex-col gap-6 p-6 600:p-12 1000:flex-row 1000:p-14">
       <div className="relative w-full 1000:flex 1000:h-full 1000:w-[340px] 1000:min-w-[340px] 1000:justify-center">
-        <div className="hideScrollbar flex w-full justify-center 1000:overflow-scroll 1000:fixed 1000:left-14 1000:top-14 1000:h-full 1000:w-[340px] 1000:max-w-[340px] 1000:pb-12 1400:left-auto">
+        <div className="hideScrollbar flex w-full justify-center 1000:fixed 1000:left-14 1000:top-14 1000:h-full 1000:w-[340px] 1000:max-w-[340px] 1000:overflow-scroll 1000:pb-12 1400:left-auto">
           {renderFilter()}
         </div>
       </div>
+      {/* <div className="catalog-grid"> */}
       <div className="flex h-fit w-full flex-wrap justify-center gap-2 600:gap-4">
         {children}
       </div>

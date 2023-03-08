a3b0ceafaf19451287ce904027c2de5996318866
Meggle <megglebande@web.de>
2023-03-08 16:24:14 +0100

    new build

--- src/pages/privacyPolicy/PrivacyPolicy.jsx
+++ src/pages/privacyPolicy/PrivacyPolicy.jsx
@@ -41,17 +41,17 @@ const PrivacyPolicy = () => {
   // };
 
   return (
-    <div className="flex h-screen w-full flex-col items-center justify-center gap-y-2 overflow-scroll p-3">
-      <div className="h-fit w-fit">
-        <Btn
-          title="Click Me"
-          type="button"
-          uiType="primary"
-          // onClick={handleClick}
-        />
-      </div>
-    </div>
-    // <div className="w-full h-screen flex justify-center items-center">work in progress</div>
+    // <div className="flex h-screen w-full flex-col items-center justify-center gap-y-2 overflow-scroll p-3">
+    //   <div className="h-fit w-fit">
+    //     <Btn
+    //       title="Click Me"
+    //       type="button"
+    //       uiType="primary"
+    //       // onClick={handleClick}
+    //     />
+    //   </div>
+    // </div>
+    <div className="w-full h-screen flex justify-center items-center">work in progress</div>
   );
 };
 

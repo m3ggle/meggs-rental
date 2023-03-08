e9ebe76603b192ef4f9aa4b477e5305cf2f93e70
Meggle <megglebande@web.de>
2023-03-08 15:41:54 +0100

    removed privacy policy

--- src/pages/privacyPolicy/PrivacyPolicy.jsx
+++ src/pages/privacyPolicy/PrivacyPolicy.jsx
@@ -2,19 +2,20 @@ import React from "react";
 import Btn from "../../components/common/Btn";
 
 const PrivacyPolicy = () => {
-  const handleClick = async () => {};
+  // const handleClick = async () => {};
 
   return (
-    <div className="flex h-screen w-full flex-col items-center justify-center gap-y-2 overflow-scroll p-3">
-      <div className="h-fit w-fit">
-        <Btn
-          title="Click Me"
-          type="button"
-          uiType="primary"
-          onClick={handleClick}
-        />
-      </div>
-    </div>
+    // <div className="flex h-screen w-full flex-col items-center justify-center gap-y-2 overflow-scroll p-3">
+    //   <div className="h-fit w-fit">
+    //     <Btn
+    //       title="Click Me"
+    //       type="button"
+    //       uiType="primary"
+    //       onClick={handleClick}
+    //     />
+    //   </div>
+    // </div>
+    <div className="w-full h-screen flex justify-center items-center">work in progress</div>
   );
 };
 
